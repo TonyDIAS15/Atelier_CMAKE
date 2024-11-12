990dd001aac6f7a85898a046dc65f72c087534fb
TonyDIAS15 <tony.dias15@outlook.fr>
2024-11-12 15:50:11 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[]){
 		printf("%lf",r);
 	}
 	else if (strcmp(op, "sqr") == 0) {
-            	r = _sqr(atof(a), atof(b));
+            	r = _sqr(atof(a));
             	printf("%lf\n", r);
         }
 	else {printf("Erreur de parametres");}
